54945aa7b7a3d7fb5b8047871687a669d345b605
Hayden Hamilton <hayden@haydenvh.com>
2020-02-03 21:21:52 +0000

    Grub

--- .config/grub/autoinstall.sh
+++ .config/grub/autoinstall.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 cd $HOME/.config/grub/
-sudo cp background.png /boot/
+sudo cp box/* /boot/grub
+sudo cp theme.txt /boot/grub
+sudo cp background.png /boot/grub
 sudo cp grub /etc/default/grub
 sudo grub-mkconfig -o grub.cfg
 sudo cp grub.cfg /boot/grub/grub.cfg
