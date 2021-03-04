ae6a5105c3082fc0e2b5460318579ac655c8de2a
hhvn <dev@haydenvh.com>
2021-03-04 19:44:04 +0000

    .local/ascii: ehh.. pointless

--- .local/ascii/hlircnet.irc/hlircnet.convert.sh
+++
@@ -1,2 +0,0 @@
-sed 's/|/51,1|/g;s/ /90,1|/g' < hlircnet.txt > hlircnet.irc  # compatible with irssi, atleast (irssi > *)
-sed 's/|/5,1|/g;s/ /2,1|/g' < hlircnet.txt > hlircnet.irc2   # compatible with more clients
