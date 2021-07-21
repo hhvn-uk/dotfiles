58c53e4ea7db37ef28a3b4f1bcede4c82f4f2e16
hhvn <dev@hhvn.uk>
2021-07-21 14:06:07 +0100

    .s/b/{mkcd,monitors.sh,fehffile,hb,0x0,ix,rand-sig,vimbhist,whatdoido,wifioff,xman,wfnintrpaste,wpa2-brute-force,torrentdone,toxclip,srssbnbc,reminder}: remove old unused scripts

--- .scripts/bin/monitors.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output LVDS1 --primary --mode 1280x800 --pos 0x280 --rotate normal --output DP1 --off --output DP2 --off --output DP3 --off --output HDMI1 --off --output HDMI2 --off --output VGA1 --mode 1920x1080 --pos 1280x0 --rotate normal --output VIRTUAL1 --off
